b73758428907532720cf99e2b0d89ce9e5146f57
Mark Garcia <jlh340@Mayras-MacBook.local>
2016-09-19 01:48:33 -0500

    set home to #

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -59,7 +59,7 @@
 			<div class="container">
 				<nav class="fh5co-main-nav">
 					<ul>
-						<li class="fh5co-active"><a href="index.html"><span>Home</span></a></li>
+						<li class="fh5co-active"><a href="#"><span>Home</span></a></li>
 						<!--
 						<li><a href="services.html"><span>Services</span></a></li>
 						<li><a href="about.html"><span>About</span></a></li>
